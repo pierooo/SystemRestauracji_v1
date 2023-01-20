52a5a1b35f8f9727037532ff8e3221a6dfa7f930
Piotr Utrata <piotrek.utrata@gmail.com>
2023-01-20 22:00:15 +0100

    Removed unused usings

--- Firma/ViewModels/GetWorkstationDeviceLinksViewModel.cs
+++ Firma/ViewModels/GetWorkstationDeviceLinksViewModel.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections.ObjectModel;
 using System.Linq;
-using SystemRestauracji.Models.Entities;
 using SystemRestauracji.Models.EntitiesForView;
 using SystemRestauracji.ViewModels.Abstract;
 
