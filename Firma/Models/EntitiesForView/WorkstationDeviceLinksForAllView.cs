b8a7d0f47a45cea6b1170ae3984ad59c0bf99d68
Piotr Utrata <piotrek.utrata@gmail.com>
2023-01-20 21:58:10 +0100

    Added workstationsdevicelinks displaying

---
+++ Firma/Models/EntitiesForView/WorkstationDeviceLinksForAllView.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemRestauracji.Models.EntitiesForView
+{
+    public class WorkstationDeviceLinksForAllView
+    {
+        public int Id { get; set; }
+        public string WorkstationName { get; set; }
+        public string WorkstationDescription { get; set; }
+        public string DeviceName { get; set; }
+        public string DeviceDescription { get; set; }
+    }
+}
