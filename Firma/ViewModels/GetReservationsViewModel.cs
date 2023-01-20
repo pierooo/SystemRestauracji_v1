59b33702c075bf1e9e20aab51d3e960de09bdbc0
Piotr Utrata <piotrek.utrata@gmail.com>
2023-01-20 15:58:43 +0100

    Added reservations displaying

---
+++ Firma/ViewModels/GetReservationsViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+using SystemRestauracji.Models.EntitiesForView;
+using SystemRestauracji.ViewModels.Abstract;
+
+namespace SystemRestauracji.ViewModels
+{
+    public class GetReservationsViewModel : ViewModelBase<ReservationForAllView>
+    {
+        public GetReservationsViewModel() : base("Rezerwacje")
+        {
+        }
+
+        public override void Load()
+        {
+            List = new ObservableCollection<ReservationForAllView>(restaurantEntities.Reservations
+                .Select(x =>
+                new ReservationForAllView()
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                    Description = x.Description,
+                    EmployeeFullName = x.Users.FirstName + " " + x.Users.LastName,
+                    ReservationDate = x.ReservationDate,
+                    OrderAmountLimit = x.OrderAmountLimit,
+                    RestaurantTableName = x.RestaurantTables.Name,
+                    NumberOfPeople = x.NumberOfPeople,
+                    ReservationStatus = x.ReservationStatus
+                }));
+        }
+    }
+}
