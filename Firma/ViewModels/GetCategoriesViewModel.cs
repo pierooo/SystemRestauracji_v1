d709a89cdb60d0612a7c9af4343e3a79a012cfa2
Piotr Utrata <piotrek.utrata@gmail.com>
2023-01-20 13:23:20 +0100

    Added Categories displaying

---
+++ Firma/ViewModels/GetCategoriesViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+using SystemRestauracji.Models.Entities;
+using SystemRestauracji.ViewModels.Abstract;
+
+namespace SystemRestauracji.ViewModels
+{
+    public class GetCategoriesViewModel : ViewModelBase<Categories>
+    {
+        public GetCategoriesViewModel() : base("Kategorie")
+        {
+        }
+
+        public override void Load()
+        {
+            List = new ObservableCollection<Categories>(restaurantEntities.Categories.Select(x => x));
+        }
+    }
+}
