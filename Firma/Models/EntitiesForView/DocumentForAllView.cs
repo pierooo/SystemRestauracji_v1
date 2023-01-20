281c18a3a0202dfa07fe2a4d77b22a57dfff3103
Piotr Utrata <piotrek.utrata@gmail.com>
2023-01-20 23:16:12 +0100

    Added Documents displaying

---
+++ Firma/Models/EntitiesForView/DocumentForAllView.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SystemRestauracji.Models.EntitiesForView
+{
+    public class DocumentForAllView
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string InvoiveName { get; set; }
+        public string AuthorFullName { get; set; }
+        public string AuthorNIP { get; set; }
+        public string RecipientFullName { get; set; }
+        public string RecipientNIP { get; set; }
+        public DateTime? PaymentDateLimit { get; set; }
+        public decimal? TotalAmountGross { get; set; }
+        public string DocumentStatus { get; set; }
+        public string EmployeeFullName { get; set; }
+        public DateTime? DocumentDate { get; set; }
+    }
+}
