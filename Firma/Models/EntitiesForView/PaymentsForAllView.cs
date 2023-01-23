1264276ef3858a9ac635ed3ab77c3328cdd43e1b
Tomasz Kowalczyk <TomaszKowalczykAGH@gmail.com>
2023-01-23 19:41:54 +0100

    Added GetPaymentsViews: Fee, Done, Open

---
+++ Firma/Models/EntitiesForView/PaymentsForAllView.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SystemRestauracji.Models.EntitiesForView
+{
+    public class PaymentsForAllView
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string EmployeeFullName { get; set; }
+        public DateTime? PaymentDate { get; set; }
+        public DateTime? PaymentDateLimit { get; set; }
+        public decimal? TotalAmountGross { get; set; }
+        public string DeviceName { get; set; }
+        public string PaymentStatus { get; set; }
+        public string PaymentType { get; set; }
+        public DateTime? LastModified{ get; set; }
+    }
+}
+
