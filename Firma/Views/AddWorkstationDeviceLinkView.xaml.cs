3b73ce420347dd1944ce2430d7adfcd1b9229ffd
Tomasz Kowalczyk <TomaszKowalczykAGH@gmail.com>
2023-01-25 21:28:13 +0100

    Added Workstationdevicelinkview adding (view can be open in only one workstation).

---
+++ Firma/Views/AddWorkstationDeviceLinkView.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace SystemRestauracji.Views
+{
+    /// <summary>
+    /// Interaction logic for AddWorkstationView.xaml
+    /// </summary>
+    public partial class AddWorkstationDeviceLinkView : AddViewBase
+    {
+        public AddWorkstationDeviceLinkView()
+        {
+            InitializeComponent();
+        }
+    }
+}
