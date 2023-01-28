b1709d87bb01c748c0ada67611aba5c302f90715
Piotr Utrata <piotrek.utrata@gmail.com>
2023-01-28 02:04:21 +0100

    Added restaurant Table adding

---
+++ Firma/ViewModels/AddRestaurantTableViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using SystemRestauracji.Models.Entities;
+using SystemRestauracji.ViewModels.Abstract;
+
+namespace SystemRestauracji.ViewModels
+{
+    public class AddRestaurantTableViewModel : ItemViewModel<RestaurantTables>
+    {
+        public string Name
+        {
+            get => Item.Name;
+            set
+            {
+                if (value != Item.Name)
+                {
+                    Item.Name = value;
+                    this.OnPropertyChanged(() => Name);
+                }
+            }
+        }
+
+        public string Description
+        {
+            get => Item.Description;
+            set
+            {
+                if (value != Item.Description)
+                {
+                    Item.Description = value;
+                    this.OnPropertyChanged(() => Description);
+                }
+            }
+        }
+
+        public AddRestaurantTableViewModel() : base("Nowy stolik")
+        {
+            base.Item = new RestaurantTables();
+        }
+
+        public override void Save()
+        {
+            Item.PhotoUrl = ImageUrl ?? ImageUrl;
+            Item.IsActive = true;
+            Item.LastModified = DateTime.Now;
+            Database.RestaurantTables.AddObject(Item);
+            Database.SaveChanges();
+        }
+    }
+}
