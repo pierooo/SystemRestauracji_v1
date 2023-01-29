e6ef4bb674f6f6380ea0f8f74368d6a4bfaee211
Piotr Utrata <piotrek.utrata@gmail.com>
2023-01-30 00:33:50 +0100

    Finished First part working for close order, need adjustment close order view and view model

---
+++ Firma/Models/Correspondences/CloseOrder.cs
@@ -0,0 +1,17 @@
+﻿using SystemRestauracji.Models.Entities;
+using SystemRestauracji.Models.EntitiesForView;
+
+namespace SystemRestauracji.Models.Correspondences
+{
+    public class CloseOrder
+    {
+        public string Action { get; set; }
+        public OrderForAllView Order { get; set; }
+
+        public CloseOrder(string action, OrderForAllView order)
+        {
+            Action = action;
+            Order = order;
+        }
+    }
+}
---
+++ Firma/ViewModels/CloseOrderViewModel.cs
@@ -0,0 +1,20 @@
+﻿using SystemRestauracji.Models.Correspondences;
+using SystemRestauracji.Models.Entities;
+using SystemRestauracji.Models.EntitiesForView;
+using SystemRestauracji.ViewModels.Abstract;
+
+namespace SystemRestauracji.ViewModels
+{
+    public class CloseOrderViewModel : ItemViewModel<Orders>
+    {
+        public OrderForAllView OrderForClose { get; set; }
+        public CloseOrderViewModel(CloseOrder orderForClose) : base("Zamknij zamówienie - " + orderForClose.Order.Name)
+        {
+            base.Item = new Orders();
+            this.OrderForClose = orderForClose.Order;
+        }
+        public override void Save()
+        {
+        }
+    }
+}
