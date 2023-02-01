877eb2e5b99c7795a2566a38646b020cbea334a1
Piotr Utrata <piotrek.utrata@gmail.com>
2023-02-01 20:15:55 +0100

    Added Second Part (view and little logic)

--- Firma/ViewModels/CloseOrderViewModel.cs
+++ Firma/ViewModels/CloseOrderViewModel.cs
@@ -1,4 +1,9 @@
-﻿using SystemRestauracji.Models.Correspondences;
+﻿using System.Linq;
+using System.Windows.Input;
+using GalaSoft.MvvmLight.Messaging;
+using SystemRestauracji.Helpers;
+using SystemRestauracji.Models.BusinessLogic;
+using SystemRestauracji.Models.Correspondences;
 using SystemRestauracji.Models.Entities;
 using SystemRestauracji.Models.EntitiesForView;
 using SystemRestauracji.ViewModels.Abstract;
@@ -7,14 +12,78 @@ namespace SystemRestauracji.ViewModels
 {
     public class CloseOrderViewModel : ItemViewModel<Orders>
     {
-        public OrderForAllView OrderForClose { get; set; }
+        private OrderForAllView orderForClose;
+        private decimal? totalPriceGross;
+
+        #region prop
+        public string OrdersForCloseLabel { get; set; }
+
+        public string Desctiption { get; set; }
+
+        public string EmployeeFullName { get; set; }
+
+        public string RestaurantTableName { get; set; }
+
+        public string PaymentName { get; set; }
+
+        public decimal? TotalPriceGross
+        {
+            get
+            {
+                return this.totalPriceGross;
+            }
+            set
+            {
+                if (value != totalPriceGross)
+                {
+                    totalPriceGross = value;
+                    base.OnPropertyChanged(() => totalPriceGross);
+                }
+            }
+        }
+        #endregion
+
+        public ICommand GetOrderDetailsCommand
+        {
+            get
+            {            
+                return new BaseCommand(GetOrderDetails);
+            }
+        }
+
+        public ICommand RefreshCommand
+        {
+            get
+            {
+                return new BaseCommand(RefreshPrice);
+            }
+        }
+
         public CloseOrderViewModel(CloseOrder orderForClose) : base("Zamknij zamówienie - " + orderForClose.Order.Name)
         {
             base.Item = new Orders();
-            this.OrderForClose = orderForClose.Order;
+            Item.Id = orderForClose.Order.Id;
+            this.orderForClose = orderForClose.Order;
+            OrdersForCloseLabel = "Zamknij zamówienie: " + orderForClose.Order.Name;
+            Desctiption = orderForClose.Order.Description;
+            EmployeeFullName = orderForClose.Order.EmployeeFullName;
+            RestaurantTableName = orderForClose.Order.RestaurantTableName;
+            PaymentName = string.IsNullOrEmpty(orderForClose.Order.PaymentName) ? "Odroczona" : orderForClose.Order.PaymentName;
+            TotalPriceGross = orderForClose.Order.TotalPriceGross;
         }
+
         public override void Save()
         {
+        }
+
+        private void GetOrderDetails()
+        {
+            Messenger.Default.Send(new OrderForOrderDetailsView(orderForClose.Id, orderForClose.Name, Status.Open));
+        }
+
+        private void RefreshPrice()
+        {
+            this.TotalPriceGross = Database.Orders.SingleOrDefault(x => x.Id == orderForClose.Id).TotalPriceGross;
         }
     }
 }
