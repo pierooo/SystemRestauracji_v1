5aab9190b0f11f5f6cc7b0badaf9e93e06eb34c7
Piotr Utrata <piotrek.utrata@gmail.com>
2023-02-09 00:52:06 +0100

    Adding document part 1

---
+++ Firma/Models/Correspondences/OrdersForDocument.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace SystemRestauracji.Models.Correspondences
+{
+    public class OrdersForDocument
+    {
+        public List<int> OrderIds { get; set; }
+
+        public string PaymentType { get; set; }
+
+        public bool IsForInvoice { get; set; }
+
+        public OrdersForDocument(List<int> orderIds, string paymentType, bool isForInvoice)
+        {
+            OrderIds = orderIds;
+            PaymentType = paymentType;
+            IsForInvoice = isForInvoice;
+        }
+    }
+}
---
+++ Firma/ViewModels/AddDocumentViewModel.cs
@@ -0,0 +1,17 @@
+﻿using SystemRestauracji.Models.Entities;
+using SystemRestauracji.ViewModels.Abstract;
+
+namespace SystemRestauracji.ViewModels
+{
+    public class AddDocumentViewModel : ItemViewModel<Documents>
+    {
+        public AddDocumentViewModel() : base("Wystaw rachunek")
+        {
+            base.Item = new Documents();
+        }
+
+        public override void Save()
+        {
+        }
+    }
+}
